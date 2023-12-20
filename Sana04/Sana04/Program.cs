62bf8e2160e4156b4997cb0a1324b161ab622f3b
Artem Levchenko <evgn.main@gmail.com>
2023-12-20 10:26:49 +0200

    Array generated

--- Sana04/Sana04/Program.cs
+++ Sana04/Sana04/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var height = 5;
+var width = 5;
+var array = new int[height, width];
+var random = new Random();
+
+for (int i = 0; i < height; i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < width; j++)
+    {
+        array[i, j] = random.Next(-10, 10);
+        Console.Write("{0,4:D}", array[i, j]);
+    }
+}
