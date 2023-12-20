6a98469b6a4153179e1779a2b8095c15a5085b54
Artem Levchenko <evgn.main@gmail.com>
2023-12-20 19:40:36 +0200

    Max element

--- Sana04/Sana04/Program.cs
+++ Sana04/Sana04/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 var height = 5;
 var width = 5;
 var array = new int[height, width];
@@ -10,7 +11,66 @@ for (int i = 0; i < height; i++)
     Console.WriteLine();
     for (int j = 0; j < width; j++)
     {
-        array[i, j] = random.Next(-10, 10);
+        array[i, j] = random.Next(-30, 30);
         Console.Write("{0,4:D}", array[i, j]);
     }
+}
+Console.WriteLine();
+
+CountPositive(array);
+
+FindMaxRepeating(array, height, width);
+
+static void FindMaxRepeating(int[,] matrix, int height, int width)
+{
+    int maxRepeatingElement = int.MinValue;
+    
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            int currentElement = matrix[i, j];
+            
+            if (IsRepeating(matrix, height, width, currentElement))
+            {
+                if (currentElement > maxRepeatingElement)
+                {
+                    maxRepeatingElement = currentElement;
+                }
+            }
+        }
+    }
+
+    Console.WriteLine("Max: {0}", maxRepeatingElement);
+}
+
+static bool IsRepeating(int[,] matrix, int height, int width, int element)
+{
+    int count = 0;
+    
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            if (matrix[i, j] == element)
+            {
+                count++;
+            }
+        }
+    }
+    return count > 1;
+}
+
+static void CountPositive(int[,] matrix)
+{
+    var count = 0;
+    foreach (var number in matrix)
+    {
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    
+    Console.WriteLine("Positive numbers: {0}", count);
 }
